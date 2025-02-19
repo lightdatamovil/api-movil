d4faa89bfe7a15ae86712e58a006d57e55e3ec82
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-02-19 14:20:34 -0300

    Refactor QR routes: rename endpoints, adjust parameter validation, and streamline response handling

--- routes/qr.js
+++ routes/qr.js
@@ -6,6 +6,29 @@ import { verifyParamaters } from '../src/funciones/verifyParameters.js';
 
 const qr = express.Router();
 
+qr.post('/driver-list', verifyToken, async (req, res) => {
+
+    const mensajeError = verifyParamaters(req.body, [], true);
+
+    if (mensajeError) {
+        return res.status(400).json({ message: mensajeError });
+    }
+
+    const { companyId } = req.body;
+
+    try {
+        const company = await getCompanyById(companyId);
+
+        const result = await driverList(company);
+
+        // crearLog(companyId, 0, "/api/listadochoferes", { estadoRespuesta: true, body: Atemp, mensaje: "" }, userId, idDispositivo, modelo, marca, versionAndroid, versionApp);
+
+        res.status(200).json({ body: result, message: "Datos obtenidos correctamente" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 qr.post('/cross-docking', async (req, res) => {
 
     const mensajeError = verifyParamaters(req.body, ['dataQr'], true);
@@ -42,29 +65,6 @@ qr.post('/get-shipment-id', async (req, res) => {
         const response = await getShipmentIdFromQr(dataQr, company);
 
         res.status(200).json({ body: response, message: "Datos obtenidos correctamente" });
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-qr.post('/driver-list', verifyToken, async (req, res) => {
-
-    const mensajeError = verifyParamaters(req.body, ['dataQr'], true);
-
-    if (mensajeError) {
-        return res.status(400).json({ message: mensajeError });
-    }
-
-    const { companyId } = req.body;
-
-    try {
-        const company = await getCompanyById(companyId);
-
-        const result = await driverList(company);
-
-        // crearLog(companyId, 0, "/api/listadochoferes", { estadoRespuesta: true, body: Atemp, mensaje: "" }, userId, idDispositivo, modelo, marca, versionAndroid, versionApp);
-
-        res.status(200).json({ body: result, message: "Datos obtenidos correctamente" });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
