af62bffa86fa839485cfd291874fecbac6af647b
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-02-25 15:37:18 -0300

    Fix parameter verification logic in verifyParameters function to use 'in' operator for better accuracy

--- src/funciones/verifyParameters.js
+++ src/funciones/verifyParameters.js
@@ -4,8 +4,7 @@ export function verifyParamaters(body, parametrosRequeridos, userData = false) {
     if (userData) {
         param.push('companyId', 'userId', 'profile');
     }
-
-    const faltantes = param.filter(p => !body[p]);
+    const faltantes = param.filter(p => !(p in body))
 
     if (faltantes.length > 0) {
         return `Faltan los siguientes parámetros: ${faltantes.join(', ')}`;
