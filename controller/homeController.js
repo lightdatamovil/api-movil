8dbc3a3268638c42fb1cd226c5518d4adbeaaad5
Agustt123 <agustintracheskyoficial@gmail.com>
2025-03-18 14:15:45 -0300

    dsadad

--- controller/homeController.js
+++ controller/homeController.js
@@ -126,8 +126,10 @@ export async function getHomeData(company, userId, profile) {
         const lineas = await executeQuery(dbConnection, "SELECT envios, envios_historial FROM tablas_indices WHERE fecha = DATE_SUB(?, INTERVAL 7 DAY) ORDER BY id DESC", [hoy]);
         let lineaEnvios;
         let lineaEnviosHistorial;
-console.log(lineas, "lineas");
+console.log(lineas, "lineas"
+);
 
+console.log(hoy, "hoy");
 
         if (lineas == undefined) {
 
