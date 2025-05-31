83518611d7d11ccff66d2f67b47a5aa4cbdbfeda
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-05-31 11:26:47 -0300

    fix: add userId parameter to fsetestadoMasivoDesde function for improved context in queries

--- controller/home/start_route.js
+++ controller/home/start_route.js
@@ -43,7 +43,7 @@ export async function startRoute(company, userId, dateYYYYMMDDHHSS, deviceFrom) 
     }
 }
 
-async function fsetestadoMasivoDesde(connection, shipmentIds, deviceFrom, dateYYYYMMDDHHSS) {
+async function fsetestadoMasivoDesde(connection, shipmentIds, deviceFrom, dateYYYYMMDDHHSS, userId) {
     try {
         const onTheWayState = 2;
         const query1 = `
@@ -55,17 +55,17 @@ async function fsetestadoMasivoDesde(connection, shipmentIds, deviceFrom, dateYY
 
         const query2 = `
             UPDATE envios
-            SET estado_envio =?
+            SET estado_envio = ?
             WHERE superado = 0 AND did IN(${shipmentIds.join(',')})
         `;
         await executeQuery(connection, query2, [onTheWayState]);
 
         const query3 = `
             INSERT INTO envios_historial (didEnvio, estado, quien, fecha, didCadete, desde)
-            SELECT did, ?, quien, ?, choferAsignado, ?
+            SELECT did, ?, ?, ?, choferAsignado, ?
             FROM envios WHERE did IN(${shipmentIds.join(',')})
         `;
-        await executeQuery(connection, query3, [onTheWayState, dateYYYYMMDDHHSS, deviceFrom]);
+        await executeQuery(connection, query3, [onTheWayState, userId, dateYYYYMMDDHHSS, deviceFrom]);
     } catch (error) {
         throw error;
     }
