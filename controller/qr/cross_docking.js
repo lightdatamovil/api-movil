c36de01a0f2c9b60632cbdf7fd28349d58296d49
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-06-23 17:31:40 -0300

    refactor: simplify shipmentId assignment and clean up query structure in crossDocking function

--- controller/qr/cross_docking.js
+++ controller/qr/cross_docking.js
@@ -9,54 +9,30 @@ export async function crossDocking(dataQr, company) {
     dbConnection.connect();
 
     try {
-        let shipmentId;
+        let shipmentId = await getShipmentIdFromQr(dataQr, company.did);
         let queryWhereId = '';
         const isLocal = dataQr.hasOwnProperty("local");
 
         if (isLocal) {
-            shipmentId = dataQr.did;
-
-            if (company.did != dataQr.empresa) {
-                const queryEnviosExteriores = `
-                    SELECT didLocal
-                    FROM envios_exteriores
-                    WHERE didExterno = ?
-                    AND didEmpresa = ?
-                `;
-                const resultQueryEnviosExteriores = await executeQuery(dbConnection, queryEnviosExteriores, [shipmentId, company.did]);
-
-                if (resultQueryEnviosExteriores.length == 0) {
-                    throw new CustomException({
-                        title: "Error en crossDocking",
-                        message: "El envío no pertenece a la empresa"
-                    });
-                }
-
-                shipmentId = resultQueryEnviosExteriores[0];
-            }
             queryWhereId = `WHERE e.did = ${shipmentId} AND e.superado = 0 AND e.elim = 0`;
         } else {
-            shipmentId = dataQr.id;
             queryWhereId = `WHERE e.superado = 0 AND e.elim = 0 AND e.ml_shipment_id = ${shipmentId}`;
         }
 
         const queryEnvios = `
- SELECT
-    e.estado_envio AS shipmentState,
-    e.didCliente AS clientId,
-    e.didEnvioZona AS zoneId,
-    DATE_FORMAT(e.fecha_inicio, '%d/%m/%Y') AS date,
-    CONCAT(su.nombre, ' ', su.apellido) AS driver
-FROM envios AS e
-LEFT JOIN envios_asignaciones AS ea
-    ON ea.didEnvio = e.did AND ea.superado = 0 AND ea.elim = 0
-LEFT JOIN sistema_usuarios AS su
-    ON ea.operador = su.did AND su.superado = 0 AND su.elim = 0
-${queryWhereId}
-LIMIT 1
-
-
-       
+            SELECT
+                e.estado_envio AS shipmentState,
+                e.didCliente AS clientId,
+                e.didEnvioZona AS zoneId,
+                DATE_FORMAT(e.fecha_inicio, '%d/%m/%Y') AS date,
+                CONCAT(su.nombre, ' ', su.apellido) AS driver
+            FROM envios AS e
+            LEFT JOIN envios_asignaciones AS ea
+                ON ea.didEnvio = e.did AND ea.superado = 0 AND ea.elim = 0
+            LEFT JOIN sistema_usuarios AS su
+                ON ea.operador = su.did AND su.superado = 0 AND su.elim = 0
+            ${queryWhereId}
+            LIMIT 1
         `;
         const envioData = await executeQuery(dbConnection, queryEnvios, []);
 
