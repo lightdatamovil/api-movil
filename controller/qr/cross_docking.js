c4c37899445c62e64b0092523114e59019f45341
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-06-25 11:01:09 -0300

    fix: ensure shipmentId is assigned as a string in crossDocking function for proper handling

--- controller/qr/cross_docking.js
+++ controller/qr/cross_docking.js
@@ -1,6 +1,6 @@
 import { executeQuery, getProdDbConfig, getZonesByCompany, getClientsByCompany } from "../../db.js";
 import mysql2 from 'mysql2';
-import { logRed } from "../../src/funciones/logsCustom.js";
+import { logRed, logYellow } from "../../src/funciones/logsCustom.js";
 import CustomException from '../../classes/custom_exception.js';
 
 export async function crossDocking(dataQr, company) {
@@ -38,30 +38,28 @@ export async function crossDocking(dataQr, company) {
         } else {
             if (company.did == 211 && !dataQr.hasOwnProperty("sender_id")) {
                 shipmentId = dataQr;
+                queryWhereId = `WHERE e.superado=0 AND e.elim=0 AND e.ml_shipment_id = '${shipmentId}'`;
             } else {
                 shipmentId = dataQr.id;
-
+                queryWhereId = `WHERE e.superado=0 AND e.elim=0 AND e.ml_shipment_id = ${shipmentId}`;
             }
-            queryWhereId = `WHERE e.superado = 0 AND e.elim = 0 AND e.ml_shipment_id = ${shipmentId}`;
+
         }
 
         const queryEnvios = `
- SELECT
-    e.estado_envio AS shipmentState,
-    e.didCliente AS clientId,
-    e.didEnvioZona AS zoneId,
-    DATE_FORMAT(e.fecha_inicio, '%d/%m/%Y') AS date,
-    CONCAT(su.nombre, ' ', su.apellido) AS driver
-FROM envios AS e
-LEFT JOIN envios_asignaciones AS ea
-    ON ea.didEnvio = e.did AND ea.superado = 0 AND ea.elim = 0
-LEFT JOIN sistema_usuarios AS su
-    ON ea.operador = su.did AND su.superado = 0 AND su.elim = 0
-${queryWhereId}
-LIMIT 1
-
-
-       
+            SELECT
+                e.estado_envio AS shipmentState,
+                e.didCliente AS clientId,
+                e.didEnvioZona AS zoneId,
+                DATE_FORMAT(e.fecha_inicio, '%d/%m/%Y') AS date,
+                CONCAT(su.nombre, ' ', su.apellido) AS driver
+            FROM envios AS e
+            LEFT JOIN envios_asignaciones AS ea
+                ON ea.didEnvio = e.did AND ea.superado = 0 AND ea.elim = 0
+            LEFT JOIN sistema_usuarios AS su
+                ON ea.operador = su.did AND su.superado = 0 AND su.elim = 0
+            ${queryWhereId}
+            LIMIT 1
         `;
         const envioData = await executeQuery(dbConnection, queryEnvios, [], true);
 
