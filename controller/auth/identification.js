bc2b4174ea50e28dfe25f5320cee98e30cdc43f7
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-07-14 11:10:40 -0300

    refactor: update depot selection logic in identification function

--- controller/auth/identification.js
+++ controller/auth/identification.js
@@ -26,7 +26,7 @@ export async function identification(company) {
             imageBase64 = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAIAAAD8v+d+AAAAWElEQVRIDbXBAQEAAAABIP6PzgpV+QUwbGR2rqlzdkcNoiCqk73A0B9H5KLVmr4YdTiO8gaCGg8VmYWqJf2zxeI1icT24tFS0hDJ01gg7LMEx6qI3SCqA6Uq8gRJbAqioBgCRH0CpvI0dpjlGr6hQJYtsDRS0BQ==';
         }
         const hasAditionalPay = [];
-        const basicPlans = [8, 27, 22, 33, 16, 37, 41];
+        const granLogisticaPlans = [24, 35, 52];
         const depots = resultsFromDepotQuery.map(depot => ({
             id: depot.id,
             name: depot.nombre,
@@ -34,7 +34,7 @@ export async function identification(company) {
             longitude: depot.longitud,
             abreviation: 'dep',
         }));
-        const hasMultiDepot = hasAditionalPay.includes(company.did * 1) || !basicPlans.includes(company.plan * 1);
+        const hasMultiDepot = hasAditionalPay.includes(company.did * 1) || granLogisticaPlans.includes(company.plan * 1);
         const result = {
             id: company.did * 1,
             plan: company.plan * 1,
@@ -45,7 +45,7 @@ export async function identification(company) {
             colectaPro: false,
             obligatoryImageOnRegisterVisit: company.did == 108,
             obligatoryDniAndNameOnRegisterVisit: company.did == 97,
-            depots: hasMultiDepot ? depots : [depots[0]],
+            depots: hasMultiDepot ? depots : depots.length > 0 ? [depots[0]] : [],
             image: imageBase64,
         };
 
