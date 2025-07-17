ccbf1a6c0a34205885977207d12141c1b9c31929
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-07-17 18:19:17 -0300

    asd

--- controller/auth/login.js
+++ controller/auth/login.js
@@ -98,7 +98,7 @@ export async function login(username, password, company) {
       phone: user.telefono,
       token,
       houses: userHouses,
-      version: "1.0.76",
+      version: "1.0.77",
     };
   } catch (error) {
     logRed(`Error en login: ${error.stack}`);
