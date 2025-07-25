1583d05d67ee65c9aa62cd89a46af37b46f3ac45
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-07-25 09:13:13 -0300

    Revert "Add LogisticaConf class for barcode handling and enhance cross-docking logic"
    This reverts commit 72e254f67e0475675a21164ed4a88024f311e7a4.

--- api-movil.js
+++ api-movil.js
@@ -23,7 +23,7 @@ dotenv.config({ path: process.env.ENV_FILE || ".env" });
 const numCPUs = 2;
 const PORT = process.env.PORT;
 
-// TODO: PROBAR HOME, NICIAR RUTA, FINALIZAR RUTA, TRAER RUTA, GUARDAR RUTA, ARMAR QR, listado de paquetes, registro de paquetes--
+// TODO: PROBAR HOME, NICIAR RUTA, FINALIZAR RUTA, TRAER RUTA, GUARDAR RUTA, ARMAR QR--
 if (cluster.isMaster) {
     logBlue(`Proceso master ${process.pid} ejecutándose...`);
 
