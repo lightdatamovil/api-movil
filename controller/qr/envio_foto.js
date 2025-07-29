20a539a9f0229ffd58b6d2b48a3a5cd6e34cb36c
denisegonzalez24 <nissgonzalezm@gmail.com>
2025-07-29 10:56:51 -0300

    Update altaEnvioFoto function to include assignment logging and enhance error handling

--- controller/qr/envio_foto.js
+++ controller/qr/envio_foto.js
@@ -1,7 +1,7 @@
 import { getProdDbConfig, executeQuery } from "../../db.js";
 import mysql2 from "mysql2";
 import axios from "axios";
-import { logCyan, logPurple, logRed } from "../../src/funciones/logsCustom.js";
+import { logCyan, logPurple, logRed, logYellow } from "../../src/funciones/logsCustom.js";
 import CustomException from "../../classes/custom_exception.js";
 
 
@@ -20,7 +20,7 @@ export async function altaEnvioFoto(company, req) {
 
     // ajustar parametros a envio foto (usuario foto)
     const reqBody = {
-      "data": { idEmpresa: companyId, quien: userId, calle: street, numero: number, localidad: city, obs: observations, elim: 69 },
+      "data": { idEmpresa: companyId, quien: userId, calle: street, numero: number, localidad: city, obs: observations, elim: 69, lote: "envioFoto" },
     };
 
     const response = await axios.post(url, reqBody, {
@@ -30,14 +30,13 @@ export async function altaEnvioFoto(company, req) {
     });
     logPurple(`Response de altaEnvioFoto: ${JSON.stringify(response.data)}`);
 
-
     if (!response.data) {
       throw new CustomException({
         title: 'Error en altaEnvioFoto',
         message: 'No se pudo registrar el envio de foto',
       });
     } else {
-      const shipmentId = response.did;
+      const shipmentId = response.data.did;
 
       const reqBody = { image, shipmentId, companyId };
       const url = 'https://files.lightdata.app/upload_foto_envios.php';
@@ -52,6 +51,27 @@ export async function altaEnvioFoto(company, req) {
         throw new CustomException({
           title: 'Error en subida de imagen',
           message: 'No se pudo subir la imagen',
+        });
+      }
+
+      const url_assignment = `http://localhost:13000/api/asignaciones/asignar-web`;
+
+      const companyId2 = parseInt(company.did, 10);
+      const req_body_asignar = {
+        shipmentId: shipmentId, userId: userId, driverId: userId, deviceFrom: "APP NUEVA", profile: 3, companyId: companyId2, appVersion: "1.0.74",
+        brand: "samsung", model: "samsung a15", androidVersion: "14", deviceId: "UTAS34.82-106-4"
+      };
+      logCyan(`ReqBody Asignar: ${JSON.stringify(req_body_asignar)}`);
+      const response_assign = await axios.post(url_assignment, req_body_asignar, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      logPurple(`Response de asignacion de envio: ${JSON.stringify(response_assign.data)}`);
+      if (!response_assign.data) {
+        throw new CustomException({
+          title: 'Error en asignacion de envio',
+          message: 'No se pudo asignar el envio',
         });
       }
       return shipmentId;
