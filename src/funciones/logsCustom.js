febd3073b7f5e96e25bbdcd47ed75f858e8c9333
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-10-03 10:44:54 -0300

    Remove console logging for log functions in logsCustom.js

--- src/funciones/logsCustom.js
+++ src/funciones/logsCustom.js
@@ -5,38 +5,38 @@ import os from "os";
 const isLocal = true;
 
 export function logGreen(message) {
-    if (isLocal) {
-        console.log(`\x1b[32m%s\x1b[0m`, `✅ ${message}
---------------------------------------------------`);
-    }
+    //     if (isLocal) {
+    //         console.log(`\x1b[32m%s\x1b[0m`, `✅ ${message}
+    // --------------------------------------------------`);
+    //     }
 }
 
 export function logRed(message) {
-    if (isLocal) {
-        console.log(`\x1b[31m%s\x1b[0m`, `❌ ${message}
---------------------------------------------------`);
-    }
+    //     if (isLocal) {
+    //         console.log(`\x1b[31m%s\x1b[0m`, `❌ ${message}
+    // --------------------------------------------------`);
+    //     }
 }
 
 export function logBlue(message) {
-    if (isLocal) {
-        console.log(`\x1b[34m%s\x1b[0m`, `🔵 ${message}
---------------------------------------------------`);
-    }
+    //     if (isLocal) {
+    //         console.log(`\x1b[34m%s\x1b[0m`, `🔵 ${message}
+    // --------------------------------------------------`);
+    //     }
 }
 
 export function logYellow(message) {
-    if (isLocal) {
-        console.log(`\x1b[33m%s\x1b[0m`, `⚠️  ${message}
---------------------------------------------------`);
-    }
+    //     if (isLocal) {
+    //         console.log(`\x1b[33m%s\x1b[0m`, `⚠️  ${message}
+    // --------------------------------------------------`);
+    //     }
 }
 
 export function logPurple(message) {
-    if (isLocal) {
-        console.log(`\x1b[35m%s\x1b[0m`, `💜 ${message}
---------------------------------------------------`);;
-    }
+    //     if (isLocal) {
+    //         console.log(`\x1b[35m%s\x1b[0m`, `💜 ${message}
+    // --------------------------------------------------`);;
+    //     }
 }
 
 export function logCyan(message) {
