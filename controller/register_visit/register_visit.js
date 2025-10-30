580e12cdd1764a0e52ab4dee7216d99c384b7b14
denu <nissgonzalezm@gmail.com>
2025-10-30 14:09:48 -0300

    fix: add company.did == 350 condition for wynflex delivery check

--- controller/register_visit/register_visit.js
+++ controller/register_visit/register_visit.js
@@ -51,7 +51,7 @@ export async function registerVisit(
 
     // Para wynflex si esta entregado
     if (
-      company.did == 72 || company.did == 125
+      company.did == 72 || company.did == 125 || company.did == 350
     ) {
       const queryEnvios =
         "SELECT didCliente, didCuenta, flex FROM envios WHERE superado = 0 AND elim = 0 AND did = ?";
