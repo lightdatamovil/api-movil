2788a12bc371876c919104e4e7a7431344bd00c6
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-11-12 12:20:49 -0300

    refactor: improve data mapping in saveRoute function for clarity

--- controller/collect/save_route.js
+++ controller/collect/save_route.js
@@ -28,21 +28,23 @@ export async function saveRoute({ db, req, company }) {
         returnRow: true,
         returnSelect: "did",
         quien: userId,
+        log: true
     });
     console.log("Ruta guardada con ID:", ruta.did);
     await LightdataORM.upsert({
         db,
         table: "colecta_ruta_paradas",
-        where: { didRuta: ruta.did },
-        versionKey: "didRuta",
-        data: clientsWithWarehouse.map((client) => ({
+        where: { didRuta: ruta.did },   // encuentra todas las paradas vigentes de esa ruta
+        data: clientsWithWarehouse.map(c => ({
             didRuta: ruta.did,
-            didCliente: client.didCliente,
-            didDeposito: client.didDeposito,
-            orden: client.orden,
-            demora: client.ordenLlegada
+            didCliente: c.didCliente,
+            didDeposito: c.didDeposito,
+            orden: c.orden,
+            demora: c.ordenLlegada,
+            quien: userId,
         })),
         quien: userId,
+        onlyReinsertProvided: true,      // ⬅️ no reinsertar las que ya no vienen en data
     });
 
     return { message: "Ruta guardada correctamente" };
