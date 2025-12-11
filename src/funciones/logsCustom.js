fa611a9d6f84caf0591138d05156431f36929c33
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-12-11 16:49:17 -0300

    fix: uncomment logging statements in logsCustom.js for improved message visibility

--- src/funciones/logsCustom.js
+++ src/funciones/logsCustom.js
@@ -5,10 +5,10 @@ import os from "os";
 const isLocal = true;
 
 export function logGreen(message) {
-    // if (isLocal) {
-    //     console.log(`\x1b[32m%s\x1b[0m`, `✅ ${message}
-    // --------------------------------------------------`);
-    // }
+    if (isLocal) {
+        console.log(`\x1b[32m%s\x1b[0m`, `✅ ${message}
+    --------------------------------------------------`);
+    }
 }
 
 export function logRed(message) {
@@ -26,22 +26,22 @@ export function logBlue(message) {
 }
 
 export function logYellow(message) {
-    // if (isLocal) {
-    //     console.log(`\x1b[33m%s\x1b[0m`, `⚠️  ${message}
-    // --------------------------------------------------`);
-    // }
+    if (isLocal) {
+        console.log(`\x1b[33m%s\x1b[0m`, `⚠️  ${message}
+    --------------------------------------------------`);
+    }
 }
 
 export function logPurple(message) {
-    // if (isLocal) {
-    //     console.log(`\x1b[35m%s\x1b[0m`, `💜 ${message}
-    // --------------------------------------------------`);;
-    // }
+    if (isLocal) {
+        console.log(`\x1b[35m%s\x1b[0m`, `💜 ${message}
+    --------------------------------------------------`);;
+    }
 }
 
 export function logCyan(message) {
-    // if (isLocal) {
-    //     console.log(`\x1b[36m%s\x1b[0m`, `💎 ${message}
-    // --------------------------------------------------`);
-    // }
+    if (isLocal) {
+        console.log(`\x1b[36m%s\x1b[0m`, `💎 ${message}
+    --------------------------------------------------`);
+    }
 }
